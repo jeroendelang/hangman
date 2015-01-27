76c83ced0c8f701bf34d6416b2a89344b20b308e
jeroendelang <jeroendelang@live.com>
2015-01-27 17:39:10 +0100

    20150127 18:00

--- scripts/js/gameController.js
+++ scripts/js/gameController.js
@@ -1,16 +1,49 @@
-﻿function gameController($scope,$http) {
+﻿function gameController($scope, $http) {
     $scope.word = "";
-    $scope.solution = "";
-    $http.get("/hangman/scripts/php/word.php")
-        .success(function (response) {
-            $scope.result = response;
-            $scope.solution = $scope.result.word;
-            setWord();
-        });
-    function setWord() {
-        $scope.word = "";
-        for (i = 1; 1 < $scope.solution.length;i++) {
-            $scope.word = $scope.word + ".";
+    $scope.gameInProgress = false;
+    $scope.guess = "";
+    $scope.tries = 11;
+    $scope.percentage = $scope.tries / 11 * 100;
+
+    function guessLetter() {
+        letters = "abncdefghijklmnopqrstuvwxyz";
+        $scope.guess = $scope.guess.toLowerCase();
+        if (letters.indexOf($scope.guess == -1)) 
+        {
+            $scope.guess = "";            
+        } else {
+            if ($scope.solution.indexOf($scope.guess == -1)) {
+                $scope.tries--;
+            } else {
+                /*
+                Als de letter wel voorkomt
+                */
+            }
         }
     }
+    function setWord() {
+        $scope.word = "";
+        
+        for (i = 0; i < $scope.solution.length; i++) {
+            $scope.word = $scope.word + ".";
+        }
+        
+    }
+    function newGame() {
+        console.log("Start a new game");
+        $http.get("/hangman/scripts/php/word.php")
+            .success(function (data, status, headers, config) {
+                console.log("Got a word from the webservice, status: " + status);
+                $scope.result = data;
+                $scope.solution = data.word;
+                setWord();
+                $scope.gameInProgress = true;
+            })
+            .error(function (data, status, headers, config) {
+                console.log("Error getting a word from the webservice, status: " + status);
+            });
+        $scope.progressType = "success";
+        $scope.percentage = 0;
+    }
+    newGame();
 }
