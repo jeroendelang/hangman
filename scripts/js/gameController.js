4b71faa17bf75fc9874ab7459a36ea2efd08c524
jeroendelang <jeroendelang@live.com>
2015-01-27 12:43:41 +0100

    20150127 12:00
    Added php JSON service to get a rondom word from a list of 274908 words
    in a text file.

---
+++ scripts/js/gameController.js
@@ -0,0 +1,16 @@
+﻿function gameController($scope,$http) {
+    $scope.word = "";
+    $scope.solution = "";
+    $http.get("/hangman/scripts/php/word.php")
+        .success(function (response) {
+            $scope.result = response;
+            $scope.solution = $scope.result.word;
+            setWord();
+        });
+    function setWord() {
+        $scope.word = "";
+        for (i = 1; 1 < $scope.solution.length;i++) {
+            $scope.word = $scope.word + ".";
+        }
+    }
+}
---
+++ scripts/php/word.php
@@ -0,0 +1,21 @@
+﻿<?php
+header('Content-type: application/json');
+
+$file = fopen("../../resources/words.english","r");
+$random = mt_rand(1,274908);
+$counter = 1;
+$word = "";
+
+while(! feof($file))
+  {
+	$word = fgets($file);
+	if ($counter == $random) {
+		$json = ["word" => trim($word)];
+		break;
+	}
+	$counter ++;
+  }
+
+fclose($file);
+echo json_encode($json);
+?>
