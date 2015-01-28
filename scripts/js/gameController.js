5717caad04b73c53fb49310bfb07e37f4a255f40
jeroendelang <jeroendelang@live.com>
2015-01-28 11:58:37 +0100

    20150128
    Game works, but needs an end

--- scripts/js/gameController.js
+++ scripts/js/gameController.js
@@ -1,33 +1,48 @@
-﻿function gameController($scope, $http) {
+﻿var app = angular.module("hangmanApp", []);
+
+app.controller("gameController", function ($scope, $http) {
     $scope.word = "";
     $scope.gameInProgress = false;
     $scope.guess = "";
+    $scope.guesses = "";
     $scope.tries = 11;
-    $scope.percentage = $scope.tries / 11 * 100;
+    $scope.percentage = 0;
 
-    function guessLetter() {
-        letters = "abncdefghijklmnopqrstuvwxyz";
-        $scope.guess = $scope.guess.toLowerCase();
-        if (letters.indexOf($scope.guess == -1)) 
-        {
-            $scope.guess = "";            
-        } else {
-            if ($scope.solution.indexOf($scope.guess == -1)) {
-                $scope.tries--;
+    $scope.guessLetter = function () {
+        // Het teken dat geraden moet worden in kleine letters zetten en eventuele spaties verwijderen
+        $scope.guess = $scope.guess.toLowerCase().trim();
+        console.log("Letter: " + $scope.guess);
+
+        // Eerste vraag: komt het gevraagde teken voor in het alfabet?
+        if ($scope.guess.match(/[a-z]/i)) {
+            // Staat blijkbaar in het alfabet, dus toevoegen aan de historie
+            $scope.guesses += $scope.guess;
+            if ($scope.solution.indexOf($scope.guess) != -1) {
+                setWord();
+                console.log("komt voor");
             } else {
-                /*
-                Als de letter wel voorkomt
-                */
+                // Letter komt niet voor, aantal resterende pogingen verminderen
+                $scope.tries--;
+                console.log("Komt niet voor, " + $scope.tries + " pogingen over.");
+                $scope.percentage = Math.round(100 - ($scope.tries / 11 * 100));
             }
+        } else {
+            // Geen processing nodig, ongeldig teken gebruikt
+            console.log("ongeldig teken, geen aftrek");
         }
+        $scope.guess = "";
     }
     function setWord() {
         $scope.word = "";
-        
+
         for (i = 0; i < $scope.solution.length; i++) {
-            $scope.word = $scope.word + ".";
+            if ($scope.guesses.indexOf($scope.solution.charAt(i)) != -1) {
+                $scope.word = $scope.word + $scope.solution.charAt(i);                
+            } else {
+                $scope.word = $scope.word + ".";
+            }
         }
-        
+
     }
     function newGame() {
         console.log("Start a new game");
@@ -46,4 +61,4 @@
         $scope.percentage = 0;
     }
     newGame();
-}
+});
